e88a36c3e1237946add1ce9dfb5798e1818af50f
ethan <172529131@qq.com>
2019-10-24 12:01:35 +0800

    http

---
+++ lib/demo/http/http_demo.dart
@@ -0,0 +1,127 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+class HttpDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HttpDemo'),
+        elevation: 0.0,
+      ),
+      body: HttpDemoHome(),
+    );
+  }
+}
+
+class HttpDemoHome extends StatefulWidget {
+  @override
+  _HttpDemoHomeState createState() => _HttpDemoHomeState();
+}
+
+class _HttpDemoHomeState extends State<HttpDemoHome> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchPosts()
+      .then((value) => print(value));
+    // final post = {
+    //   'title': 'hello',
+    //   'desc': 'nice to meet you'
+    // };
+    // print(post['title']);
+    // print(post['desc']);
+    // final postJson = json.encode(post);
+    // // print(postJson);
+    // final postJsonConvert = json.decode(postJson);
+    // print(postJsonConvert);
+    // print(postJsonConvert['title']);
+    // print(postJsonConvert['desc']);
+    // print(postJsonConvert is Map);
+
+    // final postModel = Post.fromJson(postJsonConvert);
+    // print('title: ${postModel.title}, description: ${postModel.desc}');
+
+    // print('${json.encode(postModel)}');
+  }
+
+  Future<List<Post>> fetchPosts() async {
+    final response = 
+        await http.get('https://resources.ninghao.net/demo/posts.json');
+    
+    // print('statusCode: ${response.statusCode}');
+    // print('body: ${response.body}');
+
+    if (response.statusCode == 200) {
+      final responseBody = json.decode(response.body);
+      List<Post> posts = responseBody['posts']
+        .map<Post>((item) => Post.fromJson(item))
+        .toList();
+
+      return posts;
+    } else {
+      throw Exception('Failed to fetch posts.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: fetchPosts(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        print('data: ${snapshot.data}');
+        print('connectionState: ${snapshot.connectionState}');
+        
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: Text('loading...'),
+          );
+        }
+
+        return ListView(
+          children: snapshot.data.map<Widget>((item) {
+            return ListTile(
+              title: Text(item.title),
+              subtitle: Text(item.author),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(item.imageUrl),
+              ),
+            );
+          }).toList(),
+        );
+      },
+    );
+  }
+}
+
+class Post {
+  final int id;
+  final String title;
+  final String desc;
+  final String author;
+  final String imageUrl;
+
+  Post( // 构造函数
+    this.id,
+    this.title,
+    this.desc,
+    this.author,
+    this.imageUrl,
+  );
+
+  Post.fromJson(Map json) // 构造函数的方法
+    : id = json['id'],
+      title = json['title'],
+      desc = json['desc'],
+      author = json['author'],
+      imageUrl = json['imageUrl'];
+
+  Map toJson() => {
+    'title': title,
+    'descritpion': desc,
+  };
+}
